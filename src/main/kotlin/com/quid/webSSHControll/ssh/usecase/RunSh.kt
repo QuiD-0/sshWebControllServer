cc360835671773c6e877fc7d1f5f6135ae179df9
QuiD-0 <wodnd101@naver.com>
2023-06-01 10:01:47 +0900

    sh 실행 유즈케이스 생성

--- src/main/kotlin/com/quid/webSSHControll/ssh/usecase/RunSh.kt
+++ src/main/kotlin/com/quid/webSSHControll/ssh/usecase/RunSh.kt
@@ -1,18 +1,19 @@
 package com.quid.webSSHControll.ssh.usecase
 
-import com.quid.webSSHControll.ssh.command.Execute
-import com.quid.webSSHControll.ssh.command.Find
+import com.quid.webSSHControll.ssh.domain.command.Execute
+import com.quid.webSSHControll.ssh.domain.command.Find
 import org.springframework.stereotype.Service
 
+@FunctionalInterface
 interface RunSh {
-    fun runSh(path: String, file: String)
+    fun execute(path: String, file: String)
 
     @Service
     class RunShUseCase : RunSh {
         private val find = Find.FindCommand()
         private val exec = Execute.ExecuteCommand()
 
-        override fun runSh(path: String, file: String) {
+        override fun execute(path: String, file: String) {
             if(!find.existFile(path, file)) throw Exception("File Not Found")
             exec.execute(path, file)
         }
