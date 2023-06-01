1b787f6547faaa21ac243aefd2d3f20fd096425e
QuiD-0 <wodnd101@naver.com>
2023-06-01 16:35:38 +0900

    property 추가

--- src/main/kotlin/com/quid/webSSHControll/ssh/domain/SshProperty.kt
+++ src/main/kotlin/com/quid/webSSHControll/ssh/domain/SshProperty.kt
@@ -3,13 +3,14 @@ package com.quid.webSSHControll.ssh.domain
 import java.util.*
 
 class SshProperty {
-
-    private val properties: Properties = Properties()
-    private val resource = javaClass.getResourceAsStream("/application.yaml")
-
     init {
         properties.load(resource)
     }
 
     fun get(key: String): String = properties.getProperty(key)
+
+    companion object{
+        private val properties: Properties = Properties()
+        private val resource = javaClass.getResourceAsStream("/application.yaml")
+    }
 }
